7cfafce6ae2772cdc427a852eb8e31639267761a
Liviu <liviutoma946@gmail.com>
2023-07-12 18:03:29 +0300

    added overview chart component

---
+++ client/src/scenes/daily/index.js
@@ -0,0 +1,11 @@
+import React, { useState, useMemo } from 'react';
+import { useTheme, Box } from '@mui/material';
+import Header from 'components/Header';
+import OverviewChart from 'components/OverviewChart';
+import { useGetSalesQuery } from 'state/api';
+
+function Daily() {
+  return <div></div>;
+}
+
+export default Daily;
